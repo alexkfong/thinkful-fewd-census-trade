ce463e8824cc986c367fc60817b78d3b3c26302e
Alex K. Fong <alexkfong@yahoo.com>
2016-01-24 23:09:51 +0800

    event string parsing added

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
 
 	// Begin by running API to get China data
+	var chinaData = getCountryDataAPI( 'china' );
 
 	//button is clicked
 	$('#navbarInterface').on( 'click', 'li', function(event) {
 
-		// Interface test
-		console.log( event.target.id );
-
+		// parse event id string for country name
+		var countryName = event.target.id.substr(6);
+		
 		// pass which button is to a function that runs API
 		// and gets country data
+		var countryData = getCountryDataAPI( countryName );
 
 		// Data received, update the DOM
 
 	});
 
 });
+
+function getCountryDataAPI( countryName ) {
+
+	return 0;
+
+};
